6553a402725c1329acc612ddc6722c98a5be49aa
krisada liangtrakool <39507939+mizziie@users.noreply.github.com>
2026-07-01 23:07:55 +0700

    add service and controller

---
+++ src/main/java/com/banking/mts/controller/TransferController.java
@@ -0,0 +1,10 @@
+package com.banking.mts.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/transfers")
+public class TransferController {
+
+}
